4c08b0385d50bb60ea2a5608debfb3ec4d4735e2
Benjamin Lu <benlumia007@users.noreply.github.com>
2022-06-01 08:44:13 -0700

    initial commit

---
+++ app/functions-filters.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Theme filters and actions.
+ * 
+ * Adds and defines custom filters and actions to the theme.
+ *
+ * @package   Inheritance
+ * @author    Benjamin Lu <benlumia007@gmail.com>
+ * @copyright 2022. Benjamin Lu
+ * @license   https://www.gnu.org/licenses/gpl-2.0.html
+ * @link      https://github.com/benlumia007/inheritance
+ */
+
+namespace Inheritance;
